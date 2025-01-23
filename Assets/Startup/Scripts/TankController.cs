5005fe5284979003de19fc3f97cf23ae5c388993
adamsandland <adam@sandlands.org>
2025-01-23 14:13:25 -0700

    Updated TankController Script for inputs

--- Assets/Startup/Scripts/TankController.cs
+++ Assets/Startup/Scripts/TankController.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
-
+using System.Collections;
 public class TankController : MonoBehaviour 
 {
     void start(){
-        GetComponent<Rigidbody2D>().position = new Vector3(-2, 2,0);
+        Vector3.position = new Vector3(-2,-1,0); //this should be changed later to take the vector3 of an empty spawn point
     }
     void Update(){
+        if(Input.GetKeyDown("Left")){
+            //start rotating the tank towards the West
+        }
 
+        if(Input.GetKeyDown("Right")){
+            //start rotating the tank towards the East
+        }
+
+        if(Input.GetKeyDown("Up")){
+            //start rotating the tank towards the North
+        }
+
+        if(Input.GetKeyDown("Down")){
+            //start rotating the tank towards the South
+        }
+
+        if(Input.GetKeyDown("G")){
+            //use G key to go forward
+        }
     }
 }
