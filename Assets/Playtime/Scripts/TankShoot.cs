b8cfd473c5bc791a3922735bfb28f15df72d45d8
adamsandland <adam@sandlands.org>
2025-02-04 13:30:12 -0700

    Functional shooting script

--- Assets/Playtime/Scripts/TankShoot.cs
+++ Assets/Playtime/Scripts/TankShoot.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class TankShoot : MonoBehaviour
 {
+    public GameObject projectile;
+    public float speed;
     void Update() 
     {
-
+        Vector3 direction=new Vector3(0,0,0);
+        GameObject bullet=null;
         if(Input.GetKeyDown("mouse 0"))
         {
-            Vector3 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            Vector3 direction = mousePosition - transform.position;
-
-            
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = mousePosition - transform.position;
+            bullet = Instantiate(projectile, transform.position, Quaternion.identity, transform);            
+        }
+        if(bullet!=null){
+            bullet.transform.position += speed*(direction)/100f;
         }
     }
 }
