71560570d94e1284dbdbbf01a655d0e135caa90a
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-02-06 13:53:13 -0700

    Basic health values and code to ensure playerhealth is not going above max health

--- Assets/Playtime/Scripts/TankInfo.cs
+++ Assets/Playtime/Scripts/TankInfo.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class TankInfo
 {
-    
+    public int maxHealth = 100;
+    public int playerHealth = 100;
+
+    void Update()
+    {
+        /*if (playerHealth > maxHealth)
+        {
+            playerHealth = maxHealth;
+        }*/
+
+        
+    }
 }
