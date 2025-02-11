8a7f2caf79e2a4c9d8c21cfe6a06555d98fd12dc
adamsandland <adam@sandlands.org>
2025-02-11 14:04:22 -0700

    Fixed tank reload time

--- Assets/Playtime/Scripts/TankShoot.cs
+++ Assets/Playtime/Scripts/TankShoot.cs
@@ -5,14 +5,21 @@ public class TankShoot : MonoBehaviour
 {
     public GameObject projectile;
     public float speed;
+    public float reload_time = 1000f;
+    float timer =1000f;
+    
     void Update() 
     {
+        
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction=mousePosition - transform.position;
         GameObject bullet=null;
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && timer>reload_time)
         {
             bullet = Instantiate(projectile,transform.position,transform.rotation);   
+            timer=0;
         }
+        timer++;
+        
     }
 }
