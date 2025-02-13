5144be485480eb8c21b8fc35978be722192caf54
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-02-13 13:10:18 -0700

    Created foundation for player dying when at or below 0 and when starting the game what health the player starts out with.

--- Assets/Playtime/Scripts/TankInfo.cs
+++ Assets/Playtime/Scripts/TankInfo.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 public class TankInfo
 {
     public int maxHealth = 100;
-    public int playerHealth = 100;
+    public int playerHealth;
 
+    void Start()
+    {
+        playerHealth = maxHealth;
+    }
     void Update()
     {
-        //Ensures player health does not exceed the starting health
-        if (playerHealth > maxHealth)
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        playerHealth -= amount;
+
+        if (playerHealth <= 0)
         {
-            playerHealth = maxHealth;
+            Destroy(gameObject);
         }
 
     }
