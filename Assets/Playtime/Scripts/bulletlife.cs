fb66a1a5d55cbb6b5e15fcfd1783230e32ee4f9a
adamsandland <adam@sandlands.org>
2025-02-25 13:42:43 -0700

    got sound working properly

--- Assets/Playtime/Scripts/bulletlife.cs
+++ Assets/Playtime/Scripts/bulletlife.cs
@@ -4,21 +4,26 @@ public class bulletlife : MonoBehaviour
 {
     public float speed = 100f;
     private Rigidbody2D rb;
-
+    public AudioSource fireSound;
     void Start()
     {
+        Instantiate(fireSound);
         rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = transform.up * speed*-1; // Moves the object in the up direction (relative to its rotation)
-        Destroy(gameObject, 3f);    
+        rb.linearVelocity = transform.up * speed * -1;
+        Destroy(gameObject, 3f);
     }
-    void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Player"){
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
             Debug.Log("Player");
         }
-        if(other.tag == "Wall"){
+
+        if (other.tag == "Wall")
+        {
             Debug.Log("Wall");
             Destroy(gameObject);
         }
     }
-
 }
