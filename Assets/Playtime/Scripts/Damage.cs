99926748bb7d6a54bf77333b5e55485e4f52c06f
cordovite <122401564+cordovite@users.noreply.github.com>
2025-02-25 13:23:33 -0700

    updated damage.cs file

--- Assets/Playtime/Scripts/Damage.cs
+++ Assets/Playtime/Scripts/Damage.cs
@@ -5,7 +5,7 @@ public class Damage : MonoBehaviour
     public int damage = 10;
     private TankInfo player;
 
-    private void OnCollisionEnter2D(Collision2d collision)
+    private void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
