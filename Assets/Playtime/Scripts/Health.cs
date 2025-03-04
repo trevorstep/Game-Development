b906e0b7b87712469d24e3bf6845bc8e974df3de
Empoweredbrush <Empoweredbrushfour@gmail.com>
2025-03-04 13:52:30 -0700

    More work on health script

--- Assets/Playtime/Scripts/Health.cs
+++ Assets/Playtime/Scripts/Health.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar : MonoBehaviour
+public class Health : MonoBehaviour
 {
     public Slider slider;
 
--- Assets/Playtime/Scripts/TankInfo.cs
+++ Assets/Playtime/Scripts/TankInfo.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class TankInfo : MonoBehaviour
 {
-
+    public int maxHealth = 100;
+    public int currentHealth;
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
 
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
     }
 
 
